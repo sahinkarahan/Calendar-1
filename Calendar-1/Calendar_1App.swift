50351b5483b37fabb3c6f9b47017b46952be1d81
sahinkarahan <sahin.karahan@hotmail.com>
2025-01-08 16:49:17 +0300

    Initial Commit

---
+++ Calendar-1/Calendar_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_1App.swift
+//  Calendar-1
+//
+//  Created by Şahin Karahan on 8.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
